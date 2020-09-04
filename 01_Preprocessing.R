8980f871bcf509903b5e3ce8e845ffbc50809133
alschel <a.v.sheludkov@gmail.com>
2020-09-04 22:14:39 +0500

    Add data on places and rewrite preprocessing

--- 01_Preprocessing.R
+++ 01_Preprocessing.R
@@ -1,17 +1,19 @@
 # Hospital Care Shrinkage in Russia
-
 # 01. Preprocessing: clean and aggregate by functional urban areas
 
 library(tidyr)
 library(dplyr)
 
-# ================
-# 1. Preprocessing
-# ================
+# ================================
+# 1. Rosstat data on hospital care
+# ================================
 
-# 1.1. Read data on bed numbers, hospitals, and population
-beds <- read.csv2("data/raw/Beds.csv", stringsAsFactors = F)
+# Read data on bed numbers, hospitals, places, and population
 # Clean the data, keep only observations with full time serie, and convert to tidy format
+
+# Койки круглосуточного стационара
+beds <- read.csv2("data/raw/Beds.csv", stringsAsFactors = F)
+
 beds %>% 
   select(FedDistrict:X2013) %>%
   filter(!is.na(X1991)) %>% 
@@ -25,6 +27,7 @@ beds %>%
          City = stringr::str_replace_all(City, " $", "")) %>%
   filter(!is.na(Beds)) -> beds
 
+# Больницы
 # hospitals <- read.csv2("data/raw/Hospitals.csv", stringsAsFactors = F)
 # hospitals %>%
 #   select(FedDistrict:X2013) %>% 
@@ -37,6 +40,20 @@ beds %>%
 #          City = stringr::str_replace_all(City, " $", "")) %>%
 #   filter(!is.na(Hospitals)) -> hospitals
 
+# Места денвного стационара
+places <- readr::read_csv("data/raw/Places.csv", 
+                          col_types = "ccciiiii") # c = character, i = integer
+places %>%
+  select(FedDistrict:`2013`) %>%
+  gather(Year, Places, `2009`:`2013`) %>%
+  mutate(Year = as.integer(Year)) %>%
+  mutate(Region = stringr::str_replace_all(Region, "Респ. ", ""),
+         Region = stringr::str_replace_all(Region, " край", ""),
+         Region = stringr::str_replace_all(Region, " $", ""),
+         City = stringr::str_replace_all(City, " $", "")) %>%
+  filter(!is.na(Places)) -> places
+
+# Численность населения
 population <- read.csv2("data/raw/Population.csv", stringsAsFactors = F)
 population %>%
   select(FedDistrict:X2013) %>% 
@@ -52,22 +69,30 @@ population %>%
   filter(!is.na(Population)) -> population
 
 # Combine data
-left_join(beds, 
-          # hospitals, 
-          population,
-          by = c("FedDistrict", "Region", "City", "Year")) -> data
+left_join(population,
+          beds, 
+          by = c("FedDistrict", "Region", "City", "Year")) %>% 
+  left_join(.,
+            places, 
+            by = c("FedDistrict", "Region", "City", "Year")) -> data
 
 # Clean data
 # data[data$City == "Ефремов" & data$Year == 2009, ]$Hospitals <- 5
 data %>% 
   mutate(City = stringr::str_replace_all(City, " \\((.*?)\\)", "")) -> data
 
-# 1.2. Read data on FUA
+# Calculate beds + places
+data %>% 
+  mutate(BedsAndPlaces = Beds + Places) -> data
+
+# =======================
+# 2. Urban Agglomerations
+# =======================
 fua <- read.csv2("data/processed/DARIUSdatabaseCottineau2014EN.csv", dec = ".",  stringsAsFactors = F) %>% 
   select(ROKATO:X2010)
 
 fua %>% 
-  filter(country == "Russia") %>%  # filter Russia
+  filter(country == "Russia") %>%
   gather(Year, Population, X1840:X2010) %>% 
   mutate(Year = stringr::str_replace_all(Year, "X", "") %>% 
            as.integer()) %>%
@@ -84,7 +109,11 @@ fua %>%
 fua %>% 
   select(ROKATO, Region_ru, region, City, long, lat, AROKATO) -> fua
 
-# 1.3. Создаем итоговый датасет
+# ========================================================================================
+# 3. Merge datasets, aggregate data by agglomerations and calculate variables of interests
+# ========================================================================================
+
+# 3.1. Merge datasets
 data %>% 
   left_join(fua, by = c("Region" = "Region_ru", "City" = "City")) -> res_data
 
@@ -99,30 +128,44 @@ res_data %>%
   mutate(Core_periphery_status = case_when(length(Population) == 1 ~ NA_character_,
                                            Population == max(Population) ~ "Core",
                                            Population != max(Population) ~ "Periphery") %>% 
-           factor(levels = c("Core", "Periphery"))) %>% # 34 ядра и 71 периферии
+           factor(levels = c("Core", "Periphery"))) %>%
   ungroup() -> res_data
 
-# Всего у нас 574 единицы в 46 регионах, объединенные в 494 AROKATO, 
-# из которых для 34 мы имеем отдельные данные для ядра и периферии
-# Самые крупные это Московская (27 единиц) и Нижегородская (12 единиц) агломерации
+# Итого, Всего у нас 710 единиц в 46 регионах, объединенные в 634 AROKATO, 
+# из которых для 38 мы имеем отдельные данные для ядра и периферии
+# Самая крупная AROKATO -  Московская (28 единиц)
 
-# =========================
-# 2. Data aggregated by FUA
-# =========================
 
-# Aggregate by FUA and label fua according the average size and annual population rate changes
+# 3.2. Aggregate by FUA 
 res_data %>% 
   group_by(AROKATO, Year) %>%
   arrange(desc(Population)) %>%   # arrange by population in descending order
   summarise(Core_city = City[1],  # pick the largest local unit
+            long = long[1],
+            lat = lat[1],
             Region = Region[1],
-            Population = sum(Population, na.rm = T), 
-            Beds = sum(Beds, na.rm = T)
-            # Hospitals = sum(Hospitals, na.rm = T)
+            region = region[1],
+            Population = sum(Population), 
+            Beds = sum(Beds),
+            Places = sum(Places),
+            BedsAndPlaces = sum(BedsAndPlaces)
             ) %>%
-  ungroup() %>%
-  mutate(Population = case_when(Population == 0 ~ NA_integer_,
-                                Population != 0 ~ Population)) %>%
+  ungroup() -> res_data_fua
+
+# Remove fua of population less than 10000 people in 2013
+res_data_fua %>%
+  group_by(AROKATO) %>%
+  arrange(desc(Year)) %>% 
+  filter(Population[1] > 10000) -> 
+  res_data_fua
+
+# Calculate bed and bed+places density (per 10000 people)
+res_data_fua %>% 
+  mutate(BedDensity = Beds / Population * 10000,
+         BedsAndPlacesDensity = BedsAndPlaces / Population *10000) -> res_data_fua
+
+# Label fua according to the average size and annual population rate changes
+res_data_fua %>%
   group_by(AROKATO) %>% 
   mutate(Mean_pop_91_13 = mean(Population, na.rm = T),
          Size_category_91_13 = case_when(Mean_pop_91_13 < 20000 ~ "<20",
@@ -130,41 +173,39 @@ res_data %>%
                                          Mean_pop_91_13 < 200000 ~ "50-200",
                                          Mean_pop_91_13 >= 200000 ~ ">200") %>% 
            factor(levels = c("<20", "20-50", "50-200", ">200")),
-         # Mean_pop_97_13 = mean(Population[Year > 1996], na.rm = T),
-         # Size_category_97_13 = case_when(Mean_pop_97_13 < 20000 ~ "<20",
-         #                                 Mean_pop_97_13 < 50000 ~ "20-50",
-         #                                 Mean_pop_97_13 < 200000 ~ "50-200",
-         #                                 Mean_pop_97_13 > 200000 ~ ">200") %>% 
-           # factor(levels = c("<20", "20-50", "50-200", ">200")),
+         Mean_pop_09_13 = mean(Population[Year >= 2009], na.rm = T),
+         Size_category_09_13 = case_when(Mean_pop_09_13 < 20000 ~ "<20",
+                                         Mean_pop_09_13 < 50000 ~ "20-50",
+                                         Mean_pop_09_13 < 200000 ~ "50-200",
+                                         Mean_pop_09_13 > 200000 ~ ">200") %>%
+         factor(levels = c("<20", "20-50", "50-200", ">200")),
          An_pop_ch = (Population - lag(Population, order_by = Year))/lag(Population, order_by = Year)*100,
          Mean_an_pop_ch_91_13 = mean(An_pop_ch, na.rm = T),
-         # Mean_an_pop_ch_97_13 = mean(An_pop_ch[Year > 1996], na.rm = T),
+         Mean_an_pop_ch_09_13 = mean(An_pop_ch[Year >= 2009], na.rm = T),
          An_pop_ch_91_13_cat = case_when(Mean_an_pop_ch_91_13 < -0.1 ~ "Shrinking",
                                          Mean_an_pop_ch_91_13 <= 0.1 ~ "Stable",
-                                         Mean_an_pop_ch_91_13 > 0.1 ~ "Growing") %>%
-           factor(levels = c("Shrinking", "Stable", "Growing"))
-         # An_pop_ch_97_13_cat = case_when(Mean_an_pop_ch_97_13 < -0.1 ~ "Shrinking",
-         #                                 Mean_an_pop_ch_97_13 <= 0.1 ~ "Stable",
-         #                                 Mean_an_pop_ch_97_13 > 0.1 ~ "Growing") %>% 
-         # factor(levels = c("Shrinking", "Stable", "Growing"))
+                                         Mean_an_pop_ch_91_13 > 0.1 ~ "Growing") %>% 
+           factor(levels = c("Shrinking", "Stable", "Growing")),
+         An_pop_ch_09_13_cat = case_when(Mean_an_pop_ch_09_13 < -0.1 ~ "Shrinking",
+                                         Mean_an_pop_ch_09_13 <= 0.1 ~ "Stable",
+                                         Mean_an_pop_ch_09_13 > 0.1 ~ "Growing") %>%
+         factor(levels = c("Shrinking", "Stable", "Growing"))
          ) -> res_data_fua
 
-
-# Calculate beds per 10000 people number and divide observations according to the bed number change rates
+# Divide observations according to the bed number change rates
 res_data_fua %>%
-  mutate(Beds_per10th = Beds/Population*10000) %>% 
   group_by(AROKATO) %>%
-  arrange(Year) %>% 
-  mutate(Beds_ch = (Beds[Year == 2013] - Beds[Year == 1991])/Beds[Year == 1991]*100,
-         Beds_per10th_ch = (Beds_per10th[Year == 2013] - Beds_per10th[Year == 1991])/Beds_per10th[Year == 1991]*100) %>% 
+  arrange(Year) %>%
+  mutate(Beds_ch = (Beds[23] - Beds[1])/Beds[1]*100,
+         BedDensity_ch = (BedDensity[23] - BedDensity[1])/BedDensity[1]*100) %>% 
   ungroup() %>%
   mutate(Beds_ch_cat = case_when(Beds_ch <= -50 ~ "Strong Decrease",
                                     Beds_ch <= -1 ~ "Decrease",
                                     Beds_ch > -1 ~ "Stable or Growing") %>%
                     factor(levels = c("Strong Decrease", "Decrease", "Stable or Growing")),
-         Beds_per10th_ch_cat = case_when(Beds_per10th_ch <= -50 ~ "Strong Decrease",
-                                         Beds_per10th_ch <= -1 ~ "Decrease",
-                                         Beds_per10th_ch > -1 ~ "Stable or Growing") %>%
+         BedDensity_ch_cat = case_when(BedDensity <= -50 ~ "Strong Decrease",
+                                         BedDensity <= -1 ~ "Decrease",
+                                         BedDensity > -1 ~ "Stable or Growing") %>%
            factor(levels = c("Strong Decrease", "Decrease", "Stable or Growing"))) -> res_data_fua
 
 # Save to Rdata file
---
+++ 03_1_Exploratory_Analysis.R
@@ -0,0 +1,6 @@
+# Exploratory Analysis (version 2)
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(colorblindr)
